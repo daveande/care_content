414d1d7a4613c8379c12db0f0f9c420c11cf0958
David Anderson <david@picketreport.com>
2012-06-29 09:23:45 -0400

    another postgres query change

--- app/controllers/content_files_controller.rb
+++ app/controllers/content_files_controller.rb
@@ -2,7 +2,12 @@ class ContentFilesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    content_files_filtered = ContentFile.where('service_area_id <> :service_area_id', :service_area_id => current_user.hospital.service_area_id)
+    if Rails.env.production?
+      content_files_filtered = ContentFile.where('service_area_id is ? OR service_area_id <> ?', nil, current_user.hospital.service_area_id)
+    else
+      content_files_filtered = ContentFile.where('service_area_id is not ?', current_user.hospital.service_area_id)
+    end
+
     if params[:category]
       @category = params[:category]
       @content_files = content_files_filtered.joins(:categories).where(:categories => {:id => params[:category]})
