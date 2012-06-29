3cb0529064506e05b446932c60a7fe3444659e2f
David Anderson <david@picketreport.com>
2012-06-29 08:25:04 -0400

    new query fix, again

--- app/controllers/content_files_controller.rb
+++ app/controllers/content_files_controller.rb
@@ -2,7 +2,7 @@ class ContentFilesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    content_files_filtered = ContentFile.where('service_area_id is not :service_area_id', :service_area_id => current_user.hospital.service_area_id)
+    content_files_filtered = ContentFile.where('service_area_id <> :service_area_id', :service_area_id => current_user.hospital.service_area_id)
     if params[:category]
       @category = params[:category]
       @content_files = content_files_filtered.joins(:categories).where(:categories => {:id => params[:category]})
