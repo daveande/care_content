8a81a44931dec7480f7c42529a9053c575bd585f
David Anderson <david@picketreport.com>
2012-06-28 22:11:25 -0400

    added preview text and changed description to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120619230024) do
+ActiveRecord::Schema.define(:version => 20120628233032) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -54,12 +54,13 @@ ActiveRecord::Schema.define(:version => 20120619230024) do
 
   create_table "content_files", :force => true do |t|
     t.string   "title"
-    t.text     "description"
+    t.string   "description"
     t.integer  "service_area_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "word_file"
     t.string   "dreamweaver_file"
+    t.text     "preview_text"
   end
 
   create_table "downloads", :force => true do |t|
