bce48d57ee27e014705738a8eff5ce9e5614b16a
David Anderson <david@picketreport.com>
2012-06-28 22:08:05 -0400

    respond with js and make ajax calls when sorting files

--- app/controllers/content_files_controller.rb
+++ app/controllers/content_files_controller.rb
@@ -2,12 +2,16 @@ class ContentFilesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    content_files_filtered = ContentFile.where('service_area_id is not ?', current_user.hospital.service_area_id) 
     if params[:category]
-      @content_files = ContentFile.joins(:categories).where(:categories => {:id => params[:category]})
+      @category = params[:category]
+      @content_files = content_files_filtered.joins(:categories).where(:categories => {:id => params[:category]})
     else
-      @content_files = ContentFile.all
+      @category = 'all'
+      @content_files = content_files_filtered 
     end
     @categories = Category.all
+
   end
 
   def show
