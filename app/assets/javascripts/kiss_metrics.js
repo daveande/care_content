01cb96566a9b848656d1ebdaf0a97a301a1df65e
David Anderson <daveande@gmail.com>
2012-08-07 08:40:38 -0400

    add kiss metrics to site

---
+++ app/assets/javascripts/kiss_metrics.js
@@ -0,0 +1,12 @@
+var _kmq = _kmq || [];
+var _kmk = _kmk || '089c2f1ead8bcc3fc951d2bcc6d67c3785b06286';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
