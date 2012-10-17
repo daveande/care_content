dacffb201496395271f19dacee38686fb323f89b
David Anderson <daveande@gmail.com>
2012-10-16 22:28:30 -0400

    download create refactored; using custom validations on download model to condense to .valid?

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,18 +1,28 @@
 class DownloadsController < ApplicationController  
-
+  before_filter :authenticate_user!
+  
   def create
     @content_file = ContentFile.find(params[:content_file_id])
-    if @content_file.can_download?(params[:type])
-      url = params[:type] +'_file_url'
-      flash[:notice] = "Your file has been downloaded"
-      Download.create(:user_id => current_user.id, 
-                      :content_file_id => @content_file.id, 
-                      :service_area_id => current_user.hospital.service_area_id
-                     ) unless params[:count] == 'false' 
-      redirect_to @content_file.send(url).to_s
+    url = params[:type] +'_file_url'
+
+    if params[:count] == 'true'
+      @download = Download.new(:user_id => current_user.id, 
+                              :content_file_id => @content_file.id, 
+                              :service_area_id => current_user.hospital.service_area_id
+                              )
+
+      if @download.valid?
+        flash[:notice] = "Your file has been downloaded"
+        @download.save unless params[:count] == 'false' 
+        redirect_to @content_file.send(url).to_s
+      else
+        messages = @download.errors.full_messages.join(" | ")
+        flash[:notice] = messages
+        redirect_to content_files_url
+      end
     else
-      flash[:notice] = "This file cannot be downloaded or there was a problem downloading the file"
-      redirect_to content_files_url
+      redirect_to @content_file.send(url).to_s
     end
   end
+    
 end
