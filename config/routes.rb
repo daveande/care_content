425b8e45c07ddc445f1ce4d327d9060bf0226d4d
David Anderson <daveande@gmail.com>
2012-11-04 18:10:17 -0500

    redirect to content_files_url if user is signed in and edits profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ CareContent::Application.routes.draw do
 
   devise_for :users, :skip => [:registrations]
   as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    get 'users/edit' => 'registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'registrations#update', :as => 'user_registration'
   end
 
   resources :users do
