7407d220407247e7a9d2d13fee5d76246ca31330
David Anderson <daveande@gmail.com>
2012-11-12 10:33:16 -0500

    include devise test helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,9 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
+  
+  config.include Devise::TestHelpers, :type => :controller
+  
   config.after(:all) do
     if Rails.env.test? 
       FileUtils.rm_rf(Dir["#{Rails.root}/spec/fixtures/uploads"])
