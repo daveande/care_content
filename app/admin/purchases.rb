7ffbe747544036408ed9ed4e31b75e0b1c5e5700
David Anderson <daveande@gmail.com>
2012-11-27 20:23:48 -0500

    show user email on form and set current_purchase on user when creating a new purchase

--- app/admin/purchases.rb
+++ app/admin/purchases.rb
@@ -9,4 +9,32 @@ ActiveAdmin.register Purchase do
     default_actions
   end
   
+  form do |f|
+    f.inputs "Purchases" do
+      f.input :user, :collection => User.all.map{ |user| [user.email, user.id] }
+      f.input :plan
+      f.input :agreed_to_terms
+      f.input :period_end
+      f.input :expired
+      f.buttons
+    end
+  end  
+
+  controller do
+    def create
+      @purchase = Purchase.new(params[:purchase])
+      @user = User.find(params[:purchase][:user_id])
+
+      if @purchase.save
+        @user.current_purchase = @purchase.id
+        @user.save
+        flash[:notice] = 'Purchase was successfully created.'
+        redirect_to admin_purchase_url(@purchase)
+      else
+        render :new
+      end
+    end
+
+  end
+
 end
